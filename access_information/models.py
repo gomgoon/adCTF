37c2d358c25b29675486022553e0878574904bfc
wjddnrgus878 <wjddnrgus888@naver.com>
2023-10-12 21:52:25 +0900

    2023.10.12

--- access_information/models.py
+++ access_information/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
 class GameBox(models.Model):
-    created_at = models.DateTimeField(null=True, blank=True)
-    challenge_id = models.PositiveIntegerField(null=True, blank=True)
-    team_id = models.PositiveIntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    challenge_id = models.CharField(max_length=255, null=True, blank=True)
+    team_id = models.CharField(max_length=255, null=True, blank=True)
     ip = models.CharField(max_length=255, null=True, blank=True)
     port = models.CharField(max_length=255, null=True, blank=True)
     ssh_port = models.CharField(max_length=255, null=True, blank=True)
     ssh_user = models.CharField(max_length=255, null=True, blank=True)
     ssh_password = models.CharField(max_length=255, null=True, blank=True)
     description = models.CharField(max_length=255, null=True, blank=True)
-    visible = models.BooleanField(null=True, blank=True)
     score = models.FloatField(null=True, blank=True)
+
+    visible = models.BooleanField(null=True, blank=True)
+
     is_down = models.BooleanField(null=True, blank=True)
     is_attacked = models.BooleanField(null=True, blank=True)
 
